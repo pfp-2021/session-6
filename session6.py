e4e20929a2367825286c3b2ef50000ccd21c2a97
Pepe García <pepe@pepegar.com>
2021-10-22 10:09:28 +0200

    add changes from class

--- session6.py
+++ session6.py
@@ -58,4 +58,31 @@ programs = {
 
 with open("programs.json", "w") as file:
     json.dump(programs, file)
+
+
+# %%
+
+
+with open("data/luke.json") as file:
+    luke = json.load(file)
+
+    print(luke["name"])
+    print(luke["hair_color"])
+    print(luke["mass"])
+
+# %%
+import csv
+import json
+
+with open("data/data.csv") as csv_file:
+    reader = csv.DictReader(csv_file)
+
+    # convert all rows to a list
+    data = list(reader)
+
+    with open("converted.json", "w") as json_file:
+        json.dump(data, json_file)
+
+    
+
 # %%
